64249c184d189df2d82c0a894e8ef58f8d30c20f
Vinícius Bernardo da Silva <viniciusbersilva1@gmail.com>
2026-04-26 04:15:45 -0300

    feat: implement climate limits and irrigation alerts step definitions

--- domain-clima/src/test/java/br/edu/cesar/cultivafacil/domain/clima/RunCucumberTest.java
+++ domain-clima/src/test/java/br/edu/cesar/cultivafacil/domain/clima/RunCucumberTest.java
@@ -1,6 +1,7 @@
 package br.edu.cesar.cultivafacil.domain.clima;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectPackages;
@@ -8,7 +9,8 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("br.edu.cesar.cultivafacil.domain.clima")
+@SelectPackages("feature")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.edu.cesar.cultivafacil.domain.clima")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
