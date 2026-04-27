466a6d0bf700ddecc807a1893f287500a5a62509
Vinícius José Matias Marques <vjmm@cesar-ldap.intranet>
2026-04-27 00:20:06 -0300

    fix(F-06): use SelectClasspathResource for Cucumber discovery

--- domain-cultivo/src/test/java/br/edu/cesar/cultivafacil/domain/cultivo/RunCucumberTest.java
+++ domain-cultivo/src/test/java/br/edu/cesar/cultivafacil/domain/cultivo/RunCucumberTest.java
@@ -4,12 +4,12 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("br.edu.cesar.cultivafacil.domain.cultivo")
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
